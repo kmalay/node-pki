1ef335b6a21242f8a3850be6d0dcdf99d6e36d60
kmalay <kmalay@blackhawkenterprise.com>
2018-12-28 13:41:27 -0500

    Update /signup route.

--- db/install.sql
+++ db/install.sql
@@ -1,7 +1,7 @@
 -- report supporting tables
 drop table users;
 create table users (
-  id integer primary key,
+  id serial primary key,
   cn text not null,
   first_name text not null,
   last_name text not null,
@@ -15,7 +15,7 @@ values (1, 'client1', 'Client', '1', 'client1@example.com', '555-555-5555');
 
 drop table signup;
 create table signup (
-  id integer primary key,
+  id serial primary key,
   cn text not null,
   first_name text not null,
   last_name text not null,
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -34,25 +34,27 @@ exports.signup = (req, res, next) => {
 	// Check to see if a signup record exists with the same email
 	db.query('select * from signup where email = $1', [email])
 	  .then(response => {
-      if (!response.rows || response.rows.length > 0) {
+      if (response.rows && response.rows.length > 0) {
 				console.log('Got existing user', email);
 				return res.status(422).send({ error: 'Email is already in use.' });
 			}
 
 			console.log('Email is NOT already in use.', console.log(response.rows))
-			// // If record does not exist then create it
-			// const sql = `
-			// 	insert into signup (cn, first_name, last_name, email, phone_number)
-			// 	values ($1, $2, $3, $4, $5)
-			// `;
-			//
-			// const values = [cn, firstName, lastName, email, phoneNumber];
-			//
-			// db.query(sql, values)
-			//   .then(response => {
-		  //     res.end('Account request created');
-		  //   })
-			// 	.catch(e => console.error(e.stack))
+
+			// If record does not exist then create it
+			const sql = `
+				insert into signup (cn, first_name, last_name, email, phone_number)
+				values ($1, $2, $3, $4, $5)
+			`;
+
+			const values = [cn, firstName, lastName, email, phoneNumber];
+
+			db.query(sql, values)
+			  .then(response => {
+					console.log(response.rows[0]);
+		      res.end('Account request created');
+		    })
+				.catch(e => console.error(e.stack))
     })
 	  .catch(e => console.error(e.stack))
 }
